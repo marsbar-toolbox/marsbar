c29c9ba3158aaa6ddb4a67e60cb151a909a6a8f2
Matthew Brett <matthew.brett@gmail.com>
2004-01-01 01:36:24 +0000

    Started work on moving Mars data format to object (sigh) Edits to make_contents so it can run in any directory (not only pwd) Moved pre_release scripts to release direcotory
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@78 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/release/make_contents.m
+++ marsbar/release/make_contents.m
@@ -1,6 +1,7 @@
-function make_contents(aString, flags)
+function make_contents(aString, flags, start_dir)
 % MAKECONTENTS makes Contents file in current working directory.
-%   MAKECONTENTS(STRING,FLAGS) creates a standard "Contents.m" file in the
+%   MAKECONTENTS(STRING [FLAGS [START_DIR]]) 
+%   creates a standard "Contents.m" file in the
 %   current directory by assembling the first comment (H1) line in
 %   each function found in the current working directory.  If a 
 %   "Contents.m" file exists, it is renamed to "Contents.old", before
@@ -10,10 +11,15 @@ function make_contents(aString, flags)
 %   resultant "Contents.m" file to rw-r--r-- on Unix systems.
 %
 %   FLAGS can contain none or more of
-%      'n'    - suppress path name in Contents file
+%      'n'    - suppress path name at top of Contents file
 %      'f'    - include first word of first line (excluded by default)
 %      'c'    - use filename 'contents.m' instead of 'Contents.m'
 %      'r'    - recursively list subdirectory contents also
+%      'i'    - include starting directory from file name path list
+%      'p'    - save contents file in current rather than listed directory 
+% 
+%   START_DIR can be omitted, giving a listing of current working
+%       directory, or it can specify the directory to list
 %
 % Updated 29 June 2000.
 % Revised to recurse down directories, handle options by
@@ -38,39 +44,61 @@ end
 if isempty(flags)
   flags = ' ';
 end
+if nargin < 3
+  start_dir = '';
+end
+if isempty(start_dir)
+  start_dir = pwd;
+end
 
+% parse flags
 if any(flags == 'c')
   cont_file = 'contents.m';
 else
   cont_file = 'Contents.m';
 end
-disp(['Creating "' cont_file '" in ' pwd])
-if exist([pwd filesep cont_file]) ~= 0 
-     copyfile(cont_file,[cont_file(1:end-1) 'old']);
-     delete(cont_file)
+if any(flags == 'p')
+  cont_dir = pwd;
+else
+  cont_dir = start_dir;
+end
+disp(['Creating "' cont_file '" in ' cont_dir])
+cont_path = fullfile(cont_dir, cont_file);
+if exist(cont_path, 'file') 
+     copyfile(cont_path, ...
+	      fullfile(cont_dir, [cont_file(1:end-1) 'old']));
+     delete(cont_path)
 end
 
 % Header lines
 line1 = ['% ' aString];
-fcontents = fopen(cont_file,'w'); 
+fcontents = fopen(cont_path,'w'); 
 fprintf(fcontents,'%s\n',line1);     
 if ~any(flags == 'n')
-  line2 = ['% Path ->  ' pwd];
+  line2 = ['% Path ->  ' start_dir];
   fprintf(fcontents,'%s\n',line2);     
 end
 
+% set first past flag
+flags = [flags '1'];
+
 % do write
-do_list('.', fcontents, cont_file, flags);
+do_list(start_dir, fcontents, flags);
 fclose(fcontents);
 
 % Change permissions on Contents.m file
 % only valid for Unix systems, no effect in Win32 systems
 if isunix
-  unix(['chmod go+r ' cont_file]);
+  unix(['chmod go+r ' cont_path]);
 end
 return
 
-function do_list(dirname, fcontents, cont_file, flags);
+function do_list(dirname, fcontents, flags)
+persistent START_DIR ST_D_LEN;
+if any(flags == '1') % first pass through
+  START_DIR = dirname;
+  ST_D_LEN = length(dirname) + 2; 
+end
 
 if any(flags == 'r')
   % find directories  
@@ -83,28 +111,38 @@ end
 
 % find m files
 files = what(dirname);  
-files.m  = files.m(logical(~strcmpi(files.m, cont_file)));
+
+% fix apparent bug in what function 
+files = files(1);
+
+% exclude any contents files
+files.m  = files.m(logical(~strcmpi(files.m,'contents.m')));
 if length(files.m)==0
      warning(['No m-files found in directory ' dirname])
      return
 end
 fprintf(fcontents,'%%\n'); 
 
-if strcmp(dirname, '.')
-  dirlab = dirname(2:end);
-else
-  dirlab = [dirname(3:end) filesep];
+% maybe exclude starting path from listing
+if ~any(flags == 'i')  
+  dirlab = dirname(ST_D_LEN:end);
+  if ~any(flags == '1') % not first pass
+    dirlab = [dirlab filesep];
+  end
+else % not excluding starting directory
+  dirlab = [dirname filesep];
 end
+    
 maxlen = size(char(files.m),2) + length(dirlab);
 
 % Write first lines to Contents.m if they exist
 for i = 1:length(files.m)
    fid=fopen(fullfile(files.path, files.m{i}),'r'); 
    aLine = '';
-   while(isempty(aLine))
+   while(isempty(aLine) | length(aLine) < 8)
      aLine = fgetl(fid);
    end
-   if (strcmp(aLine(1:8),'function') == 1),
+   if strcmp(aLine(1:8),'function'),
 	count_percent = 0;
 	while count_percent < 1 & feof(fid)==0; 
 	     line = fgetl(fid);
@@ -133,7 +171,8 @@ for i = 1:length(files.m)
    fclose(fid);
 end
 % recurse down directory tree
+flags = flags(flags ~= '1'); % reset first pass flag 
 for d = 1:length(dirnames)
-  do_list(fullfile(dirname, dirnames{d}), fcontents, cont_file, flags);
+  do_list(fullfile(dirname, dirnames{d}), fcontents, flags);
 end
 return
