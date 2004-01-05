1d2c464446e74e232f9ea715c262c6f8b12380e4
Matthew Brett <matthew.brett@gmail.com>
2004-01-05 17:34:24 +0000

    Summary function to marsy private function
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@86 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@marsy/summary_data.m
+++ marsbar/@marsy/summary_data.m
@@ -6,12 +6,18 @@ function [Y Yvar o] = summary_data(o, sumfunc)
 if nargin > 1
   o = sumfunc(o, sumfunc);
 end
+if isempty(sumfunc(o))
+  error('No summary function specified');
+end
 
+% refresh summary data if necessary
+% (if sumfunc passed, if data is available)
 st = o.y_struct;
 if ~isfield(st, 'Y')
   Ys = region_data(o);
   if isempty(Ys)
     error('No region data to summarize');
   end
-  Y = 
-  [st.Y st.Yvar] = pr_sum_func(
+  
+  [st.Y st.Yvar] = pr_sum_func(Ys{i}, o.sumfunc);
+end
