65b27702011d02baeb7cd1a1dce3258cd4f5527d
Matthew Brett <matthew.brett@gmail.com>
2004-01-06 07:30:12 +0000

    Region_size harmonized with summary_size (and size) call syntax
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@90 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@marsy/region_size.m
+++ marsbar/@marsy/region_size.m
@@ -1,7 +1,46 @@
-function sz = region_size(o, r_no)
+function [m,n] = region_size(o, r_no, dim)
 % method to get size of specified region data
 % 
 % $Id$ 
 
-d = region_data(o, r_no)
-sz = size(d);
+r = n_regions(o);
+if nargin < 2
+  error('Need region number');
+end
+if r_no > r
+  error('Region number too large');
+end
+
+st = y_struct(o);
+r_f = isfield(st, 'regions');
+y_f = isfield(st, 'Y');
+
+if ~r_f & ~y_f
+  error('No information for region data size');
+end
+
+r_st = [];
+if r_f
+  r_st = st.regions{r_nos(i)};
+end
+if isfield(r_st, 'Y')
+  sz = size(r_st.Y);
+elseif y_f    
+  sz = [size(st.Y, 1), 1];
+else
+  error('No data to get size for region');
+end
+
+if nargin < 3
+  if nargout > 1
+    m = sz(1); n = sz(2);
+  else
+    m = sz;
+  end
+else  
+  if dim > 2
+    m = (m+n > 0)
+  else
+    m = sz(dim);
+  end
+end
