a01adbffcf738b4f228ab0cb6af380cfcae0364c
Matthew Brett <matthew.brett@gmail.com>
2004-01-14 00:24:03 +0000

    Load private function always deblanks filenames out of politeness
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@113 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@maroi/private/my_loadroi.m
+++ marsbar/@maroi/private/my_loadroi.m
@@ -14,7 +14,7 @@ end
 
 if ischar(fname), fname = cellstr(fname);end
 for f = 1:length(fname)
-  F = load(fname{f});
+  F = load(deblank(fname{f}));
   if isfield(F, 'roi') & isa(F.roi, 'maroi')
     o(f) = F.roi;
     o(f).source = fname{f};
