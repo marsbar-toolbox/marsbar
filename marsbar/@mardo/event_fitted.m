86ac84379abb3bee3c5164c7bbadc58fc3aabc6b
Matthew Brett <matthew.brett@gmail.com>
2004-01-16 07:59:19 +0000

    Added function to return fitted event time course given duration
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@121 fb889f04-0b10-0410-b0be-90829468e382

---
+++ marsbar/@mardo/event_fitted.m
@@ -0,0 +1,49 @@
+function tc = event_fitted(D, event_spec, dur)
+% method to compute fitted event time course
+% FORMAT tc = event_fitted(D, event_spec, dur)
+% 
+% D          - design
+% event_spec - 2 by N array specifying events to combine
+%                 with row 1 giving session number
+%                 and row 2 giving event number in session
+%                 This may in due course become an object type
+% dur        - duration in seconds of event to estimate for
+% 
+% Returns
+% tc         - fitted event time course, averaged over events
+%
+% $Id$ 
+
+if nargin < 2
+  error('Need event specification');
+end
+if nargin < 3
+  dur = 0;
+end
+
+if ~is_fmri(D) | isempty(event_spec)
+  tc = [];
+  return
+end
+if ~is_mars_estimated(D)
+  error('Need a MarsBaR estimated design');
+end
+
+e_s_l = size(event_spec, 2);
+SPM   = des_struct(D);
+betas = SPM.betas;
+tc    = zeros(1, size(betas, 2));
+for e_i = 1:e_s_l
+  es    = event_spec(:, e_i);
+  ss    = es(1);
+  X     = event_regressor(D, es, dur);
+  B     = betas(event_cols(D, es), :);
+  Yh    = X*B;
+  sz    = size(Yh, 1);
+  szo   = size(tc, 1);
+  if sz > szo
+    tc(end+1:sz, :) = 0;
+  end
+  tc(1:sz,:) = tc(1:sz,:) + Yh;  
+end
+tc = tc / e_s_l;
