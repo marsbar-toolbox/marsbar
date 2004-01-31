b651d84062ca1361a7f68831d32e7313377c8435
Matthew Brett <matthew.brett@gmail.com>
2004-01-31 06:55:10 +0000

    Added optional second argument to allow find of private functions
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@246 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/mars_cvs_version.m
+++ marsbar/mars_cvs_version.m
@@ -1,13 +1,21 @@
-function str = mars_cvs_version(mfile)
+function str = mars_cvs_version(mfile, class_name)
 % returns cvs version number from m file as string
-% FORMAT str = mars_cvs_version(mfile)
-% 
+% FORMAT str = mars_cvs_version(mfile [,class_name])
+%
+% mfile      - name of function 
+% class_name - optional class name to help matlab find function
+%              (needed for class private functions)
+%
 % $Id$
 
 if nargin < 1
   error('Need matlab function or filename');
 end
-mfname = which(mfile);
+if nargin < 2
+  mfname = which(mfile);
+else
+  mfname = which(mfile, 'in', [class_name filesep class_name]);
+end
 if isempty(mfname)
   error(['Cannot find .m file: ' mfile]);
 end
