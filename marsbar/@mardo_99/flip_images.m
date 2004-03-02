32294ce27645e6759c3fb6c6695ebda0cc25e247
Matthew Brett <matthew.brett@gmail.com>
2004-03-02 21:41:45 +0000

    Removed flip_images from mardo_99, moved function into mardo object
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@326 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo_99/flip_images.m
+++
@@ -1,9 +0,0 @@
-function o = flip_images(o)
-% flips images in design
-if ~has_images(o), return, end
-M = diag([-1 1 1 1]);
-des = des_struct(o);
-for i = 1:length(des.VY)
-  des.VY(i).mat = M * des.VY(i).mat;
-end
-o = des_struct(o,des);  
