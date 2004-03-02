ff5d401616705110882b68a4b4e1952baafbe4af
Matthew Brett <matthew.brett@gmail.com>
2004-03-02 21:42:35 +0000

    Made flip_images generic across design types for image get/set
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@327 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo/flip_images.m
+++ marsbar/@mardo/flip_images.m
@@ -1,8 +1,13 @@
 function o = flip_images(o)
 % flips images in design
+%
+% $Id$
+  
 if ~has_images(o), return, end
+VY = get_images(o);
 M = diag([-1 1 1 1]);
-for i = 1:length(o.des_struct.VY)
-  o.des_struct.VY(i).mat = M *o.des_struct.VY(i).mat;
+for i = 1:length(VY)
+  VY(i).mat = M * VY(i).mat;
 end
+o = set_images(o, VY);
   
