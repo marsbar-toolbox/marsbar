04ef7c08045cb3d690aa329d2b2bb0068f2b9035
Matthew Brett <matthew.brett@gmail.com>
2004-08-11 01:09:47 +0000

    Fixed calling arguments confusion
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@337 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@marmoire/add_item.m
+++ marsbar/@marmoire/add_item.m
@@ -1,20 +1,27 @@
-function o = add(o, data, filename)
+function o = add_item(o, item_name, item_contents)
 % add item to armoire
+% FORMAT o = add_item(o, item, I)
 % 
+% o              - object
+% item_name     - item name
+% item_contents = item ... contents
+%
 % $Id $
   
 if nargin < 2
-  error('Need data to add');
+  error('Need item name to add');
 end
 if nargin < 3
-  filename = NaN;
+  item_contents = [];
 end
-
-data.name = item;
 I = default_item(o);
-def_fns = fieldnames(I);
-new_fns = def_fns(~ismember(def_fns, fieldnames(data)));
-for fn = new_fns'
-  data = setfield(data, fn{1}, getfield(I, fn{1}));
+if isempty(item_contents)
+  item_contents = I;
+else
+  def_fns = fieldnames(I);
+  new_fns = def_fns(~ismember(def_fns, fieldnames(item_contents)));
+  for fn = new_fns'
+    item_contents = setfield(item_contents, fn{1}, getfield(I, fn{1}));
+  end
 end
-o.items = setfield(o.items, I.name, I);
+o.items = setfield(o.items, item_name, item_contents);
