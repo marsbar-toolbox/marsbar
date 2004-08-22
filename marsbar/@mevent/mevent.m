27076afc82f258694d7f505a7db4bde30fd1b6b6
Matthew Brett <matthew.brett@gmail.com>
2004-08-22 06:19:23 +0000

    Added new object for managing event types
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@403 fb889f04-0b10-0410-b0be-90829468e382

---
+++ marsbar/@mevent/mevent.m
@@ -0,0 +1,25 @@
+function [o, others] = mevent(params, varargin)
+% marmoire - class constructor for mevent container type
+% FORMAT [o, others] = mevent(params, varargin)
+%  
+% $id$
+  
+myclass = 'mevent';
+defstruct = struct('event_types', []);
+
+if nargin < 1
+  params = [];
+end
+if isa(params, myclass)
+  o = params;
+  return
+end
+
+% fill with defaults, parse into fields for this object, children
+[pparams, others] = mars_struct('ffillsplit', defstruct, params);
+
+% add cvs tag
+pparams.cvs_version = mars_cvs_version(myclass);
+
+% Set as object
+o  = class(pparams, myclass);
