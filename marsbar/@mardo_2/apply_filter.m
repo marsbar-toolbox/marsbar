544089d48a8d2141dc81512be4a240f2f49fa9b0
Matthew Brett <matthew.brett@gmail.com>
2004-09-01 19:40:28 +0000

    Fixed multisession filter, when selecting single sessions
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@411 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo_2/apply_filter.m
+++ marsbar/@mardo_2/apply_filter.m
@@ -55,7 +55,8 @@ if isfield(flags, 'sessions')
       error('Sessions appear to be out of range');
     end
     K = K(ss);
-    W = W([blk_rows{ss}], :);
+    K.row = blk_rows{ss} - blk_rows{ss}(1) + 1; 
+    W = W(blk_rows{ss}, blk_rows{ss});
   end
 end
 
