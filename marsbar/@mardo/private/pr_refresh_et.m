dea4f1a2eb58c36d5b1a07068ad09aec848ece38
Matthew Brett <matthew.brett@gmail.com>
2004-09-05 04:43:49 +0000

    Added change of userdata for edit control, when doing refresh
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@429 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo/private/pr_refresh_et.m
+++ marsbar/@mardo/private/pr_refresh_et.m
@@ -1,4 +1,4 @@
-function F = pr_refresh_et(D, ic, F, hList)
+function F = pr_refresh_et(D, ic, F, hList, hEdit)
 % Refreshes data and display of event type window after edit
 % FORMAT F = pr_refresh_et(D, ic, F, hList)
 % 
@@ -6,9 +6,10 @@ function F = pr_refresh_et(D, ic, F, hList)
 % ic             - indices to events to select
 % F              - (optional) figure handle
 % hList          - (optional) handle to list uicontrol
+% hEdit          - (optional) handle to Edit uicontrol
 % 
 % Returns
-% F              - figure handle (in case you didn't have it
+% F              - figure handle (in case you didn't have it)
 %
 % $Id$
   
@@ -23,6 +24,9 @@ if nargin < 3
 end
 if nargin < 4
   hList = findobj(F, 'Tag','eList');
+end
+if nargin < 5
+  hEdit = findobj(F, 'Tag','eEdit');
 end
 
 if ~ishandle(F)
@@ -41,3 +45,4 @@ end
 set(hList, 'String', eNames);
 set(hList, 'Value', ic);
 set(F, 'Userdata', D);
+set(hEdit, 'UserData', 1);
