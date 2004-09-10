287570ca7782f4c6fc7ef921e95738e2545ed8b0
Matthew Brett <matthew.brett@gmail.com>
2004-09-10 23:31:01 +0000

    New methods to save design objects as SPM design structures
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@457 fb889f04-0b10-0410-b0be-90829468e382

---
+++ marsbar/@mardo_2/save_spm.m
@@ -0,0 +1,32 @@
+function tf = save_spm(D, fname);
+% method to save design as SPM format design structure
+% FORMAT tf = save_spm(D, fname);
+% 
+% Inputs
+% D      - design object
+% fname  - filename
+% 
+% Outputs
+% tf     - flag ==1 if successful
+% 
+% $Id$
+  
+if nargin < 2
+  fname = 'SPM.mat';
+end
+SPM = des_struct(D);
+if ~mars_utils('isabspath', fname)
+  swd = mars_struct('getifthere', SPM, 'swd');
+  if isempty(swd)
+    error('No path passed, and none in design');
+  end
+  fname = fullfile(swd, fname);
+end
+
+try 
+  save(fname, 'SPM');
+  tf = 1;
+catch
+  warning(lasterr);
+  tf = 0;
+end
---
+++ marsbar/@mardo_99/save_spm.m
@@ -0,0 +1,40 @@
+function tf = save_spm(D, fname);
+% method to save design as SPM format design structure
+% FORMAT tf = save_spm(D, fname);
+% 
+% Inputs
+% D      - design object
+% fname  - filename
+% 
+% Outputs
+% tf     - flag ==1 if successful
+% 
+% $Id$
+  
+if nargin < 2
+  if is_spm_estimated(D)
+    fname = 'SPM.mat';
+  elseif has_images(D)
+    fname = 'SPMcfg.mat';
+  elseif is_fmri(D)
+    fname = 'SPM_fMRIDesMtx.mat';
+  else
+    error('Cannot work out design type for default filename');
+  end
+end
+SPM = des_struct(D);
+if ~mars_utils('isabspath', fname)
+  swd = mars_struct('getifthere', SPM, 'swd');
+  if isempty(swd)
+    error('No path passed, and none in design');
+  end
+  fname = fullfile(swd, fname);
+end
+
+try 
+  savestruct(SPM, fname);
+  tf = 1;
+catch
+  warning(lasterr);
+  tf = 0;
+end
