71ee1df5e464ef9722e3a45ae72de2e728e93606
Matthew Brett <matthew.brett@gmail.com>
2004-09-18 18:47:52 +0000

    Specified 1 1 1 as minimum limits to sphere voxel coordinates to avoid out-of-range voxels being returned
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@478 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@maroi_sphere/voxpts.m
+++ marsbar/@maroi_sphere/voxpts.m
@@ -16,7 +16,7 @@ vox = sqrt(sum(sp.mat(1:3,1:3)'.^2));
 vr = mr ./ vox;
 vc = sp.mat \ [mc(1:3) 1]';
 vc = vc(1:3)';
-blim = [max([0 0 0; ceil(vc-vr)]); min([sp.dim(1:3); floor(vc+vr)])];
+blim = [max([1 1 1; ceil(vc-vr)]); min([sp.dim(1:3); floor(vc+vr)])];
 [R,C,P]=ndgrid(...
     blim(1,1):blim(2,1),blim(1,2):blim(2,2),blim(1,3):blim(2,3));
 vXYZ = [R(:)';C(:)';P(:)'];
