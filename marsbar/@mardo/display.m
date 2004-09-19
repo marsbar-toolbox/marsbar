dc9faa10b363b9566a74594c3f592e31ad7213fc
Matthew Brett <matthew.brett@gmail.com>
2004-09-19 03:14:13 +0000

    Report object class for display (more general for inherited objects)
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@479 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo/display.m
+++ marsbar/@mardo/display.m
@@ -2,8 +2,8 @@ function display(obj)
 % display method for mardo objects
 %
 % $Id$
-  
-src = '[MarsBaR design object]';
+
+src = ['[' class(obj) ' design object]'];
 if length(obj) > 1 % array of objects
   sz = size(obj);
   src = sprintf('%d by %d array of %s', sz(1), sz(2), src);
