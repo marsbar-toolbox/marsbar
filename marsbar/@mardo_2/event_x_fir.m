ef586511a1b52bfb622d3faea1501a3e2c9a3678
Matthew Brett <matthew.brett@gmail.com>
2004-11-17 22:36:43 +0000

    Extensive rewrite to implement flat FIR models, as well as the default SPM (stacked) models.  Also returns FIR column basis function value, as this may not be 1 (because of spm_orth) for all SPM design types.
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@525 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo_2/event_x_fir.m
+++ marsbar/@mardo_2/event_x_fir.m
@@ -1,15 +1,23 @@
-function Xn = event_x_fir(D, e_spec, bin_length, bin_no)
+function [Xn, rh] = event_x_fir(D, e_spec, bin_length, bin_no, opts)
 % method to return FIR design matrix columns for session
-% FORMAT Xn = event_x_fir(D, e_spec, bin_length, bin_no)
+% FORMAT [Xn, rh] = event_x_fir(D, e_spec, bin_length, bin_no, opts)
 % 
 % D          - design object
 % e_spec     - event specification for single event
 %                [session no; event no]
 % bin_length - bin length in seconds  [TR]
 % bin_no     - number of bins for FIR [25 seconds / bin_length]
-% 
+% opts       - structure, containing fields with options
+%                'flat' - if field present, gives flat FIR 
+%                 This option removes any duration information, and
+%                 returns a simple per onset FIR model, where ones in the
+%                 design matrix corresponds to 1 event at the given
+%                 offset. See event_fitted_fir.m for more details.
+%
 % Returns
 % Xn         - columns in design matrix for FIR model
+% rh         - regressor height: value for regressor for one event of
+%              duration dt in design matrix
 %
 % $Id$
 
@@ -22,6 +30,10 @@ end
 if nargin < 4
   bin_no = [];
 end
+if nargin < 5
+  opts = [];
+end
+
 s = e_spec(1);
 e = e_spec(2);
 if isempty(bin_length)
@@ -38,9 +50,45 @@ xBF.name    = 'Finite Impulse Response';
 xBF.order   = bin_no;
 xBF.length  = xBF.order*bin_length;
 xBF         = pr_spm_get_bf(xBF);
+rh          = xBF.bf(1,1);
 
 U           = SPM.Sess(s).U(e);
-U.u         = U.u(:,1);
-Xn          = pr_spm_volterra(U,xBF.bf,1);
 k           = SPM.nscan(s);
+
+if isfield(opts, 'flat')
+  U.u = sf_ones_ons(U, xBF, k);
+  if verbose(D)
+    if any(diff(U.dur))
+      warning(['Different event durations; ' ...
+	       'flat FIR model likely to be invalid']);
+    end
+  end
+else
+  U.u = U.u(:,1);
+end
+
+Xn          = pr_spm_volterra(U,xBF.bf,1);
 Xn          = Xn([0:(k - 1)]* xBF.T + xBF.T0 + 32,:);
+
+return
+
+function sf = sf_ones_ons(U, xBF, k)
+% Return onsets with only 1s in start time bin for each event
+  
+ons   = U.ons;
+T     = xBF.T;
+dt    = xBF.dt;
+switch xBF.UNITS
+ case 'scans'
+  TR = T*dt;
+ case 'secs'
+  TR = 1;
+end
+
+ton       = round(ons*TR/dt) + 32;
+sf        = sparse((k*T + 128),1);
+for j = 1:length(ton)
+  sf(ton(j),:) = sf(ton(j),:) + 1;
+end
+
+return
