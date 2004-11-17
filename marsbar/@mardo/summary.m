1522cba349dcb0d47ab364eeb2bc79d9df7f018e
Matthew Brett <matthew.brett@gmail.com>
2004-11-17 02:02:46 +0000

    Added SPM.swd to summary output
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@514 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo/summary.m
+++ marsbar/@mardo/summary.m
@@ -2,18 +2,19 @@ function strs = summary(D)
 % method returns cell array of strings describing design
 % 
 % $Id$
-  
-strs{1} = sprintf('Design type:       \t%s',  type(D));
-strs{2} = sprintf('Modality:          \t%s',  modality(D));
+
+strs{1} = sprintf('SPM working dir    \t%s',  swd(D));
+strs{2} = sprintf('Design type:       \t%s',  type(D));
+strs{3} = sprintf('Modality:          \t%s',  modality(D));
 if is_fmri(D)
   tmp = sf_recode(has_filter(D));
 else
   tmp = 'N/A';
 end
-strs{3} = sprintf('Has filter?:       \t%s',  tmp);
-strs{4} = sprintf('Has images?:       \t%s',  ...
+strs{4} = sprintf('Has filter?:       \t%s',  tmp);
+strs{5} = sprintf('Has images?:       \t%s',  ...
 		  sf_recode(has_images(D)));
-strs{5} = sprintf('MarsBaR estimated?:\t%s', ...
+strs{6} = sprintf('MarsBaR estimated?:\t%s', ...
 		  sf_recode(is_mars_estimated(D)));
 strs = [strs {'Description:'}, descrip(D)];
 
