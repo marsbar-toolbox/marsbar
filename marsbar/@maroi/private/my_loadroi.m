644e004d520b4acbf40d8cc71709e59b4aa6e059
Matthew Brett <matthew.brett@gmail.com>
2004-11-17 02:14:40 +0000

    Deblank file names to prevent load error
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@521 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@maroi/private/my_loadroi.m
+++ marsbar/@maroi/private/my_loadroi.m
@@ -12,6 +12,7 @@ o = [];
 if iscell(fname), fname = char(fname); end
 if size(fname, 1) > 1, error('Can only load one ROI at a time'); end
 if isempty(fname), warning('Empty filename'), return, end
+fname = deblank(fname);
 F = load(fname);
 if isfield(F, 'roi') & isa(F.roi, 'maroi')
   o = F.roi;
