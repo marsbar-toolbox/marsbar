f44b1f6da627464e5a2541d2d4dfce0eacdc9714
Matthew Brett <matthew.brett@gmail.com>
2004-11-17 22:35:38 +0000

    Extensive rewrite to implement flat FIR models, as well as the default SPM (stacked) models.  Also returns FIR column basis function value, as this may not be 1 (because of spm_orth).  Also, works only on the first column of the stick function for selected event, to avoid problem with parametric regressors.
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@524 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/@mardo_99/event_x_fir.m
+++ marsbar/@mardo_99/event_x_fir.m
@@ -1,16 +1,24 @@
-function Xn = event_x_fir(D, e_spec, bin_length, bin_no)
+function [Xn, rh] = event_x_fir(D, e_spec, bin_length, bin_no, opts)
 % method to return FIR design matrix columns for session
-% FORMAT Xn = event_x_fir(D, e_spec, bin_length, bin_no)
+% FORMAT [Xn, rh] = event_x_fir(D, e_spec, bin_length, bin_no, opts)
 % 
 % D          - design object
 % e_spec     - event specification for single event
 %                [session no; event no]
 % bin_length - bin length in seconds
 % bin_no     - number of bins for FIR
+% opts       - structure, containing fields with options
+%                'flat' - if field present, gives flat FIR 
+%                 This option removes any duration information, and
+%                 returns a simple per onset FIR model, where ones in the
+%                 design matrix corresponds to 1 event at the given
+%                 offset. See event_fitted_fir.m for more details.
 % 
 % Returns
 % Xn         - columns in design matrix for FIR model
-%
+% rh         - regressor height: value for regressor for one event of
+%              duration dt in design matrix
+% 
 % Note that we have a problem, in that the assumed start bin is not saved
 % in the SPM99 design format, so we have to hope it has not changed from
 % the current defaults.
@@ -32,6 +40,10 @@ end
 if nargin < 4
   bin_no = [];
 end
+if nargin < 5
+  opts = [];
+end
+
 s = e_spec(1);
 e = e_spec(2);
 if isempty(bin_length)
@@ -53,14 +65,30 @@ if recorded_fMRI_T ~= fMRI_T & verbose(D)
       'The original fMRI_T0 has not been recorded, assuming %d.'],...
 		  fMRI_T, recorded_fMRI_T, fMRI_T0));
 end
-
+T           = recorded_fMRI_T;
 bf          = kron(eye(bin_no),ones(round(bin_length/dt),1));
 BF{1}       = pr_spm_orth(bf);
-SF{1}       = Sess.sf{e};
+rh          = BF{1}(1);
 k           = length(Sess.row);
+
+if isfield(opts, 'flat')
+  [onsets durations] = event_onsets(D, e_spec);
+  ons   = sparse(k*T,1);
+  for p = 1:length(onsets)
+    q  = round(onsets(p)*T + 1);
+    ons(q) = 1;
+  end
+  SF{1} = ons(1:(k*T));
+  if verbose(D) & any(diff(durations))
+      warning(['Apparently there were different event durations; ' ...
+	       'flat FIR model likely to be invalid']);
+  end
+else
+  SF{1}       = Sess.sf{e}(:,1);
+end
 
 Xn          = pr_spm_volterra(SF,BF,{'FIR'},1);
 
 % Resample design matrix {X} at acquisition times
 %-----------------------------------------------
-Xn          = Xn([0:k-1]*recorded_fMRI_T + fMRI_T0,:);
+Xn          = Xn([0:k-1]*T + fMRI_T0,:);
