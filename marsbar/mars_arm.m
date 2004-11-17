27c61cf409b910b52f6c7fe7962bb4fe4db3fc8d
Matthew Brett <matthew.brett@gmail.com>
2004-11-17 01:53:05 +0000

    Added various extra wrappers for marmoire calls.  Added routine to display contents of item data (using summary()) method) to SPM graphics window.
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@511 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/mars_arm.m
+++ marsbar/mars_arm.m
@@ -31,12 +31,31 @@ switch lower(action)
   [o varargout{1}] = update_item_data(o, varargin{:});
  case 'set_param'
   o = set_item_param(o, varargin{:});
+ case 'get_param'
+  varargout{1} = get_item_param(o, varargin{:});
  case 'save'
   [varargout{1} o] = save_item_data(o, varargin{:});
  case 'save_ui'
   [varargout{1} o] = save_item_data_ui(o, varargin{:});
  case 'isempty'
   varargout{1} = isempty_item_data(o, varargin{:});
+ case 'item_exists'
+  varargout{1} = item_exists(o, varargin{:});
+ case 'show_summary'
+  if nargin < 2, error('Need item name'); end
+  item_name = varargin{1};
+  if ~item_exists(o, item_name)
+    error(['What is ' item_name '?']);
+  end
+  if mars_arm('isempty', item_name)
+    S = {'[Empty]'};
+  else
+    S  = summary(get_item_data(o, item_name));
+    fn = get_item_param(o, item_name, 'file_name');
+    if isempty(fn), fn = '[Not set]'; end
+    S  = [{['Filename: ' fn]} S];
+  end  
+  mars_utils('graphic_text', S, get_item_param(o, item_name, 'title'));
  otherwise
   error('Weird');
 end
