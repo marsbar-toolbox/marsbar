000850ff2424861ef2e0f9a9af8ead2950e843f9
Matthew Brett <matthew.brett@gmail.com>
2006-04-01 00:01:03 +0000

    Moved scripts for example data into marsbar distribution
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@608 fb889f04-0b10-0410-b0be-90829468e382

---
+++ marsbar/examples/batch/run_preprocess.m
@@ -0,0 +1,24 @@
+% Run smoothing and SPM analysis for MarsBaR ER sample data
+% 
+% $Id: run_preprocess.m,v 1.2 2004/08/15 01:19:43 matthewbrett Exp $ 
+
+% You might want to define the path to the example data here, as in
+% subjroot = '/my/path/somewhere';
+subjroot = spm_get(-1, '', 'Root directory of example data');
+sesses = {'sess1','sess2','sess3'};
+
+spm_v = spm('ver');
+sdirname = [spm_v '_ana'];
+if ~strcmp(spm_v, 'SPM99'), spm_defaults; end
+
+% Make sure SPM modality-specific defaults are set
+spm('defaults', 'fmri');
+
+% do smoothing
+er_smooth(subjroot, sesses, 'nu*.img', 8);
+
+% Run statistics, contrasts
+for ss = 1:length(sesses)
+  model_file = configure_er_model(subjroot, sesses{ss}, sdirname);
+  estimate_er_model(model_file, [1 0]);
+end
