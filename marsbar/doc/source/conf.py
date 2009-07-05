dba4893b20946c4cf1510aa3f8174f916c9c3573
Matthew Brett <matthew.brett@gmail.com>
2009-07-05 20:00:52 +0000

    Tuning css settings in conf file
    git-svn-id: https://marsbar.svn.sourceforge.net/svnroot/marsbar/trunk@635 fb889f04-0b10-0410-b0be-90829468e382

--- marsbar/doc/source/conf.py
+++ marsbar/doc/source/conf.py
@@ -96,7 +96,17 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "rightsidebar": "true",
+    'bgcolor': '#fffff5',
+    'footerbgcolor': '#fffff5',
+    'headtextcolor': '#C00',
+    'headbgcolor': '#fffff5',
+    'sidebarbgcolor': '#fbfbe0',
+    'sidebartextcolor': '#C00',
+    'sidebarlinkcolor': '#084B8A',
+    'relbarbgcolor': '#848484',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
