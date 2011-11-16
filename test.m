cf2c28adfad2602ec17ed8197b0383f9d09507fe
Matthew Brett <matthew.brett@gmail.com>
2011-11-15 19:23:19 -0800

    NF - add marsbar path before running tests

--- test.m
+++ test.m
@@ -1,6 +1,7 @@
 % Script to run tests
 % Be careful, this will clear all your matlab variables
 clear classes
+addpath(fullfile(pwd, 'marsbar'))
 addpath(fullfile(pwd, 'testing'))
 marsbar on
 run_tests tests
