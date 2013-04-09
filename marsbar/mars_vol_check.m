8803b01633df45c0b964b0d05c7c09407274d112
Matthew Brett <matthew.brett@gmail.com>
2013-04-09 11:04:40 -0700

    RF: use SPM8 version of spm_vol_check
    Only formatting differences between old and new version.

--- marsbar/mars_vol_check.m
+++ marsbar/mars_vol_check.m
@@ -8,37 +8,37 @@ function [samef, msg, chgf] = mars_vol_check(varargin)
 % msg              - cell array containing helpful message if not
 % chgf             - logical Nx2 array of difference flags
 %_______________________________________________________________________
-% Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
+% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Copied from spm_vol_check.m
 % Matthew Brett
-% $Id: spm_vol_check.m 184 2005-05-31 13:23:32Z john $
+% $Id: spm_vol_check.m 2696 2009-02-05 20:29:48Z guillaume $
 
 
 [fnames samef msg] = deal({},1,{});
 
 if nargin < 1,
-	return;
-end;
+    return;
+end
 
 for i = 1:numel(varargin),
-	vols   = varargin{i};
-	if ~isempty(vols),
-		if i == 1,
-			dims   = cat(3,vols(:).dim);
-			mats   = cat(3,vols(:).mat);
-		else,
-			dims   = cat(3,dims,vols(:).dim);
-			mats   = cat(3,mats,vols(:).mat);
-		end;
-		fnames = {fnames{:}, vols(:).fname};
-	end;
-end;
+    vols   = varargin{i};
+    if ~isempty(vols),
+        if i == 1,
+            dims   = cat(3,vols(:).dim);
+            mats   = cat(3,vols(:).mat);
+        else
+            dims   = cat(3,dims,vols(:).dim);
+            mats   = cat(3,mats,vols(:).mat);
+        end
+        fnames = {fnames{:}, vols(:).fname};
+    end
+end
   
 nimgs = size(dims, 3);
 if nimgs < 2,
-	return;
-end;
+    return;
+end
 
 labs = {'dimensions', 'orientation & voxel size'};
 
@@ -47,27 +47,25 @@ matf = any(any(diff(mats,1,3)));
 chgf = logical([dimf(:) matf(:)]);
 chgi = find(any(chgf, 2));
 if ~isempty(chgi),
-	samef = 0;
-	e1    = chgi(1);
-	msg   = {['Images don''t all have the same ' ...
-		  sepcat(labs(chgf(e1,:)),', ')],...
-		'First difference between image pair:',...
-		fnames{e1},...
-		fnames{e1+1}};
-end;
-return;
+    samef = 0;
+    e1    = chgi(1);
+    msg   = {['Images don''t all have the same ' ...
+          sepcat(labs(chgf(e1,:)),', ')],...
+        'First difference between image pair:',...
+        fnames{e1},...
+        fnames{e1+1}};
+end
 
 function s = sepcat(strs, sep)
 % returns cell array of strings as one char string, separated by sep
 if nargin < 2,
-	sep = ';';
+    sep = ';';
 end
 if isempty(strs),
-	s = '';
-	return;
+    s = '';
+    return
 end
 strs = strs(:)';
 strs = [strs; repmat({sep}, 1, length(strs))];
 s    = [strs{1:end-1}];
-return;
 
